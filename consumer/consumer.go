36b897df107de6657a9a4651ce4785bbf0b6c88d
priyanka mandal <priyankamandal3101@gmail.com>
2025-05-19 15:18:00 +0530

    consumercomments

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -2,51 +2,51 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
+	"os"        //used for interacting with the oprerating system
+	"os/signal" //captures the os signals
+	"syscall"   //used to define specific os signals
 
 	"github.com/IBM/sarama"
 )
 
 func main() {
-	topic := "comments"
-	worker, err := connectConsumer([]string{"localhost:29092"})
+	topic := "comments"                                         //sets the kafka topic name to listen to comments
+	worker, err := connectConsumer([]string{"localhost:29092"}) //Calls the connectConsumer function to connect to Kafka running at localhost:29092.
 	if err != nil {
 		panic(err)
 	}
-	consumer, err := worker.ConsumePartition(topic, 0, sarama.OffsetOldest)
+	consumer, err := worker.ConsumePartition(topic, 0, sarama.OffsetOldest) //Creates a new consumer for the specified topic and partition. It starts consuming from the oldest message available.
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("consumer started")
-	sigchan := make(chan os.Signal, 1)
-	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
-	msgCount := 0
-	doneCh := make(chan struct{})
+	sigchan := make(chan os.Signal, 1)                      //prepare to listen for OS signals
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM) //registers the signals we want to listen for
+	msgCount := 0                                           //Tracks how many messages received.
+	doneCh := make(chan struct{})                           //used to stop the program safely                      //Used to stop the program safely.
 	go func() {
 		for {
 			select {
-			case err := <-consumer.Errors():
+			case err := <-consumer.Errors(): //Prints any errors from Kafka.
 				fmt.Println(err)
 			case msg := <-consumer.Messages():
-				msgCount++
-				fmt.Printf("Received message Count: %d: | Topic (%s) | Message(%s)n", msgCount, string(msg.Topic), string(msg.Value))
+				msgCount++                                                                                                             //Increases message count.
+				fmt.Printf("Received message Count: %d: | Topic (%s) | Message(%s)\n", msgCount, string(msg.Topic), string(msg.Value)) //Prints the topic and the message received.
 			case <-sigchan:
 				fmt.Println("Interrruption detection")
-				doneCh <- struct{}{}
+				doneCh <- struct{}{} //When user interrupts, it prints a message and sends a signal to stop the goroutine.
 				return
 			}
 		}
 	}()
-	<-doneCh
-	fmt.Println("Processed", msgCount, "message")
-	if err := worker.Close(); err != nil {
+	<-doneCh                                      //Waits until the signal is received to shut down the program.
+	fmt.Println("Processed", msgCount, "message") //Shows how many messages were processed before stopping.
+	if err := worker.Close(); err != nil {        //Closes the Kafka consumer connection safely.
 		panic(err)
 	}
 }
-func connectConsumer(brokersUrl []string) (sarama.Consumer, error) {
-	config := sarama.NewConfig()
+func connectConsumer(brokersUrl []string) (sarama.Consumer, error) { //Creates a new Kafka consumer connection.
+	config := sarama.NewConfig() //Tells kafka to return errors using config.Consumer.Return.Errors = true.
 	config.Consumer.Return.Errors = true
 	conn, err := sarama.NewConsumer(brokersUrl, config)
 	if err != nil {
